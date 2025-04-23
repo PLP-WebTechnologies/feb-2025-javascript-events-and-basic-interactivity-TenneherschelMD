6bf30c48fa88c43a956816f65c85c6eaed53133d
TenneherschelMD <willysotieno6@students.uonbi.ac.ke>
2025-04-23 03:03:13 +0300

    Add files via upload

---
+++ Week6 AssignmentHTML/gallery.js
@@ -0,0 +1,2 @@
+document.getElementById("myH").textContent ="JS-EVENT-ASSIGNMENT";
+
---
+++ Week6 AssignmentHTML/index.js
@@ -0,0 +1,13 @@
+document.getElementById("myH").textContent ="JS-EVENT-ASSIGNMENT";
+
+document.getElementById("myH1").textContent ="Intro into this js-event-assignment";
+document.getElementById("myP1").textContent ="Welcome to our interactive web experience! This project is designed to engage users with interactive elements, fun animations, and real-time feedback. Whether you're clicking buttons, navigating through an image gallery, or submitting a form, we've made sure each feature adds something special to the experience.";
+
+document.getElementById("myH2").textContent = "Button Interaction";
+document.getElementById("myP2").textContent = "Have fun interacting with the button below! When clicked, the button changes its text and background color, giving you immediate feedback. It's a simple but effective way to add dynamic behavior to your web pages.";
+
+document.getElementById("myH3").textContent = "Image Gallery/Slideshow, Form Validation & Interactive Fun Features";
+document.getElementById("myP3").textContent = "Explore our collection of images with the slideshow! Each image is displayed for a few seconds before transitioning to the next one. This feature is great for showcasing multiple images in a limited space and keeping your site visually dynamic.Need to submit some information? Our form validation ensures that all required fields are filled out correctly. You'll get immediate feedback as you type, whether it's ensuring a password meets the minimum length or confirming that an email address is formatted properly. We've added these features to make sure your data is always correct before submission! We've also included fun, interactive features like keypress detection and hover effects. For example, if you press the Enter key on your keyboard, you'll see a special alert. Hover over the button to see a cool color change. It's all about making the experience engaging and enjoyable for users.";
+
+document.getElementById("myH4").textContent = "Outro out of this js-event-assignment";
+document.getElementById("myP4").textContent = "Thank you for checking out our interactive bundle of joy! We hope these features make your web browsing experience more enjoyable. Don't forget to try all the interactive elements and explore the full functionality of the page. We're always working to improve and add new features, so stay tuned for more!";
